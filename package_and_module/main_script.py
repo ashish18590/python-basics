87f615b509ed931f93604163dc896f3a653ba994
ashish3 <ashish@intel.com>
2018-07-17 12:36:14 +0530

    [package_and_module]:	Meaning of __name__ varible in python
    	- This commit explains meaning of __name__ varible in python.
    	- Case 1 :
    		if script is running directly then its value will be "__main__"

---
+++ package_and_module/main_script.py
@@ -0,0 +1,11 @@
+'''
+	This is main or master script.
+	Example Run:
+		python main_script.py
+'''
+
+
+if __name__ == "__main__":
+	print("main_script.py is running *****directly*****")
+else:
+	print("main_script.py is imported in another script")
